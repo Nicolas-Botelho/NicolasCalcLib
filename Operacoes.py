b93f6da4dac7cee9a36097bfe7bc428ddbf75be6
Nicolas <nicolasbotelho2005@gmail.com>
2025-04-30 09:36:59 -0300

    operacoes

---
+++ Operacoes.py
@@ -0,0 +1,26 @@
+def soma(num1: float, num2: float) -> float:
+    return num1 + num2
+
+def subs(num1: float, num2: float) -> float:
+    return num1 - num2
+
+def mult(num1: float, num2: float) -> float:
+    return num1 * num2
+
+def divs(num1: float, num2: float) -> float:
+    if (num2 == 0):
+        # throw divByZeroExcepion
+    
+    return num1 / num2
+
+def expo(num1: float, num2: float) -> float:
+    return num1 ** num2
+
+def exp2(num1: float) -> float:
+    return num1 ** 2
+
+def raiz(num1: float, num2: float) -> float:
+    return num1 ** (1/num2)
+
+def rzqd(num1: float) -> float:
+    return num1 ** (1/2)
